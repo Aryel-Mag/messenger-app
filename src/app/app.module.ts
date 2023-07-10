ee61523b2c619f7d827a59f778cfa81a5dd14069
Ariel Victorio Magnane <magsmacbook@AMP.local>
2023-07-10 14:01:09 +0200

    project's structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProfileBannerComponent } from './profile-banner/profile-banner.component';
+import { ChatBannerComponent } from './chat-banner/chat-banner.component';
+import { ChatMainComponent } from './chat-main/chat-main.component';
+import { ChatListComponent } from './chat-list/chat-list.component';
+import { WriterComponent } from './writer/writer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProfileBannerComponent,
+    ChatBannerComponent,
+    ChatMainComponent,
+    ChatListComponent,
+    WriterComponent
   ],
   imports: [
     BrowserModule,
