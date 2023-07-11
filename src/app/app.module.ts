a2a07d6dd4691ca2e3846ff9c52739f57c4b41d4
Ariel Victorio Magnane <magsmacbook@AMP.local>
2023-07-11 08:59:25 +0200

    structure being done with materials

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,18 @@ import { ChatBannerComponent } from './chat-banner/chat-banner.component';
 import { ChatMainComponent } from './chat-main/chat-main.component';
 import { ChatListComponent } from './chat-list/chat-list.component';
 import { WriterComponent } from './writer/writer.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatAutocompleteModule } from '@angular/material/autocomplete'
+import { MatDividerModule } from '@angular/material/divider';
+import { MatListModule } from '@angular/material/list';
+
+
 
 @NgModule({
   declarations: [
@@ -20,7 +32,15 @@ import { WriterComponent } from './writer/writer.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatGridListModule,
+    MatFormFieldModule,
+    MatInputModule,
+    ReactiveFormsModule,
+    MatAutocompleteModule,
+    MatDividerModule,
+    MatListModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
