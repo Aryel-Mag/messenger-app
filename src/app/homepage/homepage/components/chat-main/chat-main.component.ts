2021460296902fe86ec51f5c5ee82a77830bbea8
Ariel Victorio Magnane <magsmacbook@amp.home>
2023-07-21 13:24:51 +0200

    Auto-scroll on messages done, fixed an issue where the last message on chat-list component wasn't being updated when messages were sent

--- src/app/homepage/homepage/components/chat-main/chat-main.component.ts
+++ src/app/homepage/homepage/components/chat-main/chat-main.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {AfterViewInit, Component, ElementRef, QueryList, ViewChild, ViewChildren} from '@angular/core';
 import {Observable, Subject, Subscription, switchMap} from "rxjs";
 import {Store} from "@ngrx/store";
 import Room from "../../../../models/roomModel";
@@ -14,11 +14,16 @@ import User from "../../../../models/userModel";
   templateUrl: './chat-main.component.html',
   styleUrls: ['./chat-main.component.scss']
 })
-export class ChatMainComponent {
+export class ChatMainComponent implements AfterViewInit {
+  @ViewChild('scrollframe', {static: false}) scrollFrame!: ElementRef;
+  @ViewChildren('message') itemElements!: QueryList<any>;
+
+  private scrollContainer: any;
+  private isNearBottom: boolean = true;
+
   public room$!: Observable<Room>;
 
   public sub1!: Subscription;
-  public sub2!: Subscription;
   public sub3!: Subscription;
 
   public user$!: Observable<User>;
@@ -29,9 +34,7 @@ export class ChatMainComponent {
     private _socketService: SocketService,
   ) { }
 
-  ngOnInit() {
-    let rooms: Observable<Room[]>;
-
+  ngAfterViewInit() {
     this.room$ = this._store.select(selectRoom);
 
     this.user$ = this._store.select(selectUser);
@@ -43,6 +46,33 @@ export class ChatMainComponent {
       this._store.dispatch(RoomsActions.addMessage({message: message}));
     });
 
+    this.scrollContainer = this.scrollFrame.nativeElement;
+    this.itemElements.changes.subscribe(_ => this.onMessageElementChange());
+  }
+
+  private onMessageElementChange(): void {
+    if (this.isNearBottom) {
+      this.scrollToBottom();
+    }
+  }
+
+  private scrollToBottom(): void {
+    this.scrollContainer.scroll({
+      top: this.scrollContainer.scrollHeight,
+      left: 0,
+      behavior: 'smooth'
+    });
+  }
+
+  private isUserNearBottom(): boolean {
+    const threshold = 150;
+    const position = this.scrollContainer.scrollTop + this.scrollContainer.offsetHeight;
+    const height = this.scrollContainer.scrollHeight;
+    return position > height - threshold;
+  }
+
+  scrolled(event: any): void {
+    this.isNearBottom = this.isUserNearBottom();
   }
 
   ngOnDestroy() {
